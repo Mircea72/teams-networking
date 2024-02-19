6c0be624e9beec599a7633a6a078488fee82429b
Mircea72 <mircris36@yahoo.com>
2024-02-19 21:58:58 +0200

    repair index js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './style.css';
-console.info('app ready');
+
+console.warn('app ready');
+
+function loadTeams() {
+  const promise = fetch('http://localhost:3000/teams-json')
+    .then(r => r.json())
+    .then(teams => {
+      console.table(teams);
+      return teams;
+    });
+
+  console.warn('loadTeams', promise);
+}
+
+loadTeams();
